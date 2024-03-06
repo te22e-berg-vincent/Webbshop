fc6aa6c897d7676a04016aff780e5d745bc9bd27
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-03-06 14:06:42 +0100

    commit local storage till java script + nya variabler

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //array som sparar varor 
-let cartItems = [];
+let varorna = [];
 
 function addToCart(itemName, price) {
   //lägger till vara i kundvagnen
@@ -8,19 +8,38 @@ function addToCart(itemName, price) {
     name: itemName,
     price: price,
   };
-  cartItems.push(item); // lägtill vara i array
+  varorna.push(item); // lägtill vara i array
   displayCart();
 }
 function displayCart() {
   // Funktion visar totala andalet varor & priser i kundvagnen
-  const totalItemsElement = document.getElementById("totalItems");
-  const totalPriceElement = document.getElementById("totalPrice");
-  const totalItems = cartItems.length; //räkna ut antalet varor
+  const AntalVarorElement = document.getElementById("AntalVaror");
+  const TotalPrisElement = document.getElementById("TotalPris");
+  const AntalVaror = varorna.length; //räkna ut antalet varor
 
-  let totalPrice = 0; //räkna ut totala priset
-  cartItems.forEach((item) => {
-    totalPrice += item.price;
+  let TotalPris = 0; //räkna ut totala priset
+  varorna.forEach((item) => {
+    TotalPris += item.price;
   });
-  totalItemsElement.textContent = `Antal varor: ${totalItems}`; //antal varor
-  totalPriceElement.textContent = `Totala priset: ${totalPrice.toFixed(2)} kr`; //totala priset
+  AntalVarorElement.textContent = `Antal varor: ${AntalVaror}`; //antal varor
+  TotalPrisElement.textContent = `Totala priset: ${TotalPris.toFixed(2)} kr`; //totala priset
+
+
 }
+varorna.push(item); // lägg till vara till varorna arrayen
+  displayCart();
+  localStorage.setItem('varorna', JSON.stringify(varorna));
+  function loadCartFromStorage() // Funktion för att hämta varor från storage
+  {
+  const storedvarorna = localStorage.getItem('varorna');
+
+  if (storedvarorna) {
+      const storedItems = JSON.parse(storedvarorna); // uppdatera arrayen
+      varorna = storedItems;
+      displayCart(); // uppdatera cart displayen
+  }
+}
+
+// Call loadCartFromStorage on page load
+window.onload = loadCartFromStorage;
+
